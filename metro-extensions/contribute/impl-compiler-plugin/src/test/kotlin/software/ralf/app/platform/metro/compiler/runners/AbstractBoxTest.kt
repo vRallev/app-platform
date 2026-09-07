1dfd14984ca838624004fa6c6d78a66fa094425d
Zac Sweers <pandanomic@gmail.com>
2026-09-06 23:38:27 -0400

    Switch to more broadly compatible AbstractFirLightTreeBlackBoxCodegenTest base
    The previous one moves around in different kotlinc versions

--- metro-extensions/contribute/impl-compiler-plugin/src/test/kotlin/software/ralf/app/platform/metro/compiler/runners/AbstractBoxTest.kt
+++ metro-extensions/contribute/impl-compiler-plugin/src/test/kotlin/software/ralf/app/platform/metro/compiler/runners/AbstractBoxTest.kt
@@ -1,12 +1,11 @@
 package software.ralf.app.platform.metro.compiler.runners
 
 import org.jetbrains.kotlin.config.JvmTarget
-import org.jetbrains.kotlin.test.FirParser
 import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
 import org.jetbrains.kotlin.test.directives.CodegenTestDirectives
 import org.jetbrains.kotlin.test.directives.ConfigurationDirectives
 import org.jetbrains.kotlin.test.directives.JvmEnvironmentConfigurationDirectives
-import org.jetbrains.kotlin.test.runners.codegen.AbstractFirBlackBoxCodegenTestBase
+import org.jetbrains.kotlin.test.runners.codegen.AbstractFirLightTreeBlackBoxCodegenTest
 import org.jetbrains.kotlin.test.services.EnvironmentBasedStandardLibrariesPathProvider
 import org.jetbrains.kotlin.test.services.KotlinStandardLibrariesPathProvider
 import software.ralf.app.platform.metro.compiler.services.configureKotlinTestImports
@@ -14,7 +13,7 @@ import software.ralf.app.platform.metro.compiler.services.configureMetroImports
 import software.ralf.app.platform.metro.compiler.services.configurePlugin
 import software.ralf.app.platform.metro.compiler.services.configureTestSupportClasspath
 
-open class AbstractBoxTest : AbstractFirBlackBoxCodegenTestBase(FirParser.LightTree) {
+open class AbstractBoxTest : AbstractFirLightTreeBlackBoxCodegenTest() {
   override fun createKotlinStandardLibrariesPathProvider(): KotlinStandardLibrariesPathProvider {
     return EnvironmentBasedStandardLibrariesPathProvider
   }
